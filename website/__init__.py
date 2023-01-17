ba21324343bedc77fce1dd17fa7d80f53f7c0361
SunHashira <leobucek@icloud.com>
2023-01-17 17:01:41 +0100

    mqtt fix

--- website/__init__.py
+++ website/__init__.py
@@ -16,12 +16,12 @@ def create_app():
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     app.config['MQTT_BROKER_URL'] = MQTT_BROKER
-    app.config['MQTT_BROKER_PORT'] = 8883  # default port for non-tls connection
+    app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
     app.config['MQTT_USERNAME'] = MQTT_NAME
     app.config['MQTT_PASSWORD'] = MQTT_PASS
     app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
-    app.config['MQTT_TLS_ENABLED'] = True  # set TLS to disabled for testing purposes
-    app.config['MQTT_TLS_INSECURE'] = True
+    app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+    # app.config['MQTT_TLS_INSECURE'] = True
 
     db.init_app(app)
     mqtt.init_app(app)
