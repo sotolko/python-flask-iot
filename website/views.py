1d65fd020544e5cfbb2ad301fc4be0c93b2249db
sotolko <tomaslenart32@gmail.com>
2023-01-17 18:04:53 +0100

    da sa vytvorit ziak

--- website/views.py
+++ website/views.py
@@ -23,7 +23,14 @@ def mqtt_actions():
     if request.method == 'POST':
         message = request.form.get('mqtt-send')
         topic = request.form.get('mqtt-topic')
-        if topic != 'home/test':
+
+        if len(message) > 20:
+            flash('Sprava nesmie byt vacsia ako 20 znakov!', category='error')
+            return render_template('mqtt_actions.html', user=current_user, spravy=messages)
+        elif len(topic) > 100:
+            flash('Topic nesmie byt vacsi ako 100 znakov!', category='error')
+            return render_template('mqtt_actions.html', user=current_user, spravy=messages)
+        elif topic != 'home/test':
             flash(f'Not subscribed to topic {topic}', category='error')
             return render_template('mqtt_actions.html', user=current_user, spravy=messages)
 
@@ -36,3 +43,28 @@ def mqtt_actions():
         return redirect(url_for('views.mqtt_actions'))
 
     return render_template('mqtt_actions.html', user=current_user, spravy=messages)
+
+@views.route('/admin-create-user', methods=['GET', 'POST'])
+@login_required
+def admin_panel():
+    if request.method == 'POST':
+        first_name = request.form.get('ziak-firstname')
+        last_name = request.form.get('ziak-lastname')
+        isic_number = request.form.get('ziak-isic')
+
+        if len(first_name) > 150:
+            flash('Meno nesmie byt vacsie ako 150 znakov!', category='error')
+            return render_template('admin_create_user.html', user=current_user)
+        elif len(last_name) > 150:
+            flash('Priezvisko nesmie byt vacsie ako 150 znakov!', category='error')
+            return render_template('admin_create_user.html', user=current_user)
+        elif len(isic_number) > 20:
+            flash('ISIC nesmie byt vacsi ako 20 znakov!', category='error')
+            return render_template('admin_create_user.html', user=current_user)
+
+        new_ziak = Ziak(first_name=first_name, last_name=last_name,isic_number=isic_number)
+        db.session.add(new_ziak)
+        db.session.commit()
+        flash('Ziak vytvoreny!', category='success')
+
+    return render_template('admin_create_user.html', user=current_user)
